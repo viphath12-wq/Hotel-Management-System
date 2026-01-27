451fbf1102811cd39fe0619056d17326d27069da
@viphath12-wq <viphath12@gmail.com>
2026-01-27 19:39:33 +0700

    4

--- src/util/configurl.js
+++ src/util/configurl.js
@@ -20,7 +20,7 @@
 
 const configurl = {
   baseurl: 'https://databasevue-production.up.railway.app/api',
-  image_path: 'https://databasevue-production.up.railway.app//storage/'
+  image_path: 'https://databasevue-production.up.railway.app/storage/'
 }
 
 export default configurl
