774edf0f23b09059b83c8f171748aeaa22591829
@viphath12-wq <viphath12@gmail.com>
2026-01-27 19:20:18 +0700

    3

--- src/util/configurl.js
+++ src/util/configurl.js
@@ -5,9 +5,22 @@
 
 // export default configurl
 
+// APP_NAME=Laravel
+// APP_ENV=production
+// APP_KEY=base64:HZUxKagXci1hf2d0OUYNHnrK7oPRs1AE6u6OBJrs6uE=
+// APP_DEBUG=true
+// APP_URL=mysql-production-8781.up.railway.app
+
+// DB_CONNECTION=mysql
+// DB_HOST=mainline.proxy.rlwy.net
+// DB_PORT=18811
+// DB_DATABASE=railway
+// DB_USERNAME=root
+// DB_PASSWORD=vOvihpyYoDOLdnMITgzWVssCryOcaJXR
+
 const configurl = {
-  baseurl: 'https://mysql-production-8781.up.railway.app/api', // Production API URL
-  image_path: 'https://mysql-production-8781.up.railway.app/storage/' // Production image storage path
+  baseurl: 'https://databasevue-production.up.railway.app/api',
+  image_path: 'https://databasevue-production.up.railway.app//storage/'
 }
 
 export default configurl
