784cf8894010e6c448b32c9d4f9f0c7e60d798e0
@viphath12-wq <viphath12@gmail.com>
2026-02-10 14:12:22 +0700

    10

--- src/util/configurl.js
+++ src/util/configurl.js
@@ -1,13 +1,13 @@
-const configurl = {
-  baseurl: 'http://localhost:8000/api',
-  image_path : 'http://localhost:8000/storage/' 
-}
-
-export default configurl
-
 // const configurl = {
-//   baseurl: 'https://databasevue-production.up.railway.app/api',
-//   image_path: 'https://databasevue-production.up.railway.app/storage/'
+//   baseurl: 'http://localhost:8000/api',
+//   image_path : 'http://localhost:8000/storage/' 
 // }
 
 // export default configurl
+
+const configurl = {
+  baseurl: 'https://databasevue-production.up.railway.app/api',
+  image_path: 'https://databasevue-production.up.railway.app/storage/'
+}
+
+export default configurl
