432ad641fa5b98213cb3d9f9860d2301b33025bd
viphath12-wq <viphath12@gmail.com>
2026-02-28 23:33:00 +0700

    52

--- src/util/configurl.js
+++ src/util/configurl.js
@@ -1,13 +1,13 @@
-const configurl = {
-  baseurl: 'http://localhost:8000/api',
-  image_path : 'http://localhost:8000/storage/' 
-}
-
-export default configurl
-
 // const configurl = {
-//   baseurl: import.meta.env.VITE_BASEURL,
-//   image_path: import.meta.env.VITE_IMAGE_PATH
+//   baseurl: 'http://localhost:8000/api',
+//   image_path : 'http://localhost:8000/storage/' 
 // }
 
 // export default configurl
+
+const configurl = {
+  baseurl: import.meta.env.VITE_BASEURL,
+  image_path: import.meta.env.VITE_IMAGE_PATH
+}
+
+export default configurl
